86f62f4a562f23ae45893ecb7d09ad0dee657d1f
Wesle <Wesle@DESKTOP-VDM8N4G>
2026-01-10 14:02:52 -0300

    Enhance DemoQA practice form automation with comprehensive field filling and validation; remove unused example fixture

--- cypress/e2e/cypress/e2e/demoqa-practice-form.cy.js
+++ cypress/e2e/cypress/e2e/demoqa-practice-form.cy.js
@@ -1,19 +1,35 @@
-describe('DemoQA - Automation Practice Form', () => {
+describe('DemoQA - Practice Form', () => {
 
   beforeEach(() => {
-    cy.visit('https://demoqa.com/automation-practice-form', {
-      failOnStatusCode: false
-    })
-
-    cy.get('form').should('exist')
+    cy.visit('https://demoqa.com/automation-practice-form')
     cy.removeAds()
   })
 
-  it('Deve preencher o nome', () => {
-    cy.get('#firstName', { timeout: 10000 })
+  it('Preenche formulário', () => {
+    cy.fixture('user').then((user) => {
+      cy.get('#firstName').type(user.firstName)
+      cy.get('#lastName').type(user.lastName)
+      cy.get('#userEmail').type(user.email)
+      cy.get('label[for="gender-radio-1"]').click()
+      cy.get('#userNumber').type(user.phone)
+      cy.get('#dateOfBirthInput').click()
+      cy.get('.react-datepicker__month-select').select('July')
+      cy.get('.react-datepicker__year-select').select('1995')
+      cy.get('.react-datepicker__day--007:not(.react-datepicker__day--outside-month)').click()
+      cy.get('#currentAddress').type(user.address)
+      cy.get('#subjectsInput').type('Maths{enter}')
+      cy.get('label[for="hobbies-checkbox-1"]').click()
+      cy.get('label[for="hobbies-checkbox-3"]').click()
+      cy.get('#state').scrollIntoView().click()
+      cy.contains('NCR').click()
+      cy.get('#city').click()
+      cy.contains('Delhi').click()
+      cy.get('#submit')
       .scrollIntoView()
-      .should('be.visible')
-      .type('Wesley')
+      .click({ force: true })
+       cy.get('.modal-content').should('be.visible')
+      cy.contains('Thanks for submitting the form').should('be.visible')
+    })
   })
 
 })
